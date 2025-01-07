d51b9ddda8a29b6183812ba755f0ab2fdd39c708
Dang Vo <dangvm280205@gmail.com>
2025-01-07 13:05:30 +0700

    fetch user chats

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,8 +1,41 @@
 import AddNewUser from "./addNewUser/AddNewUser";
 import "./chatList.css"
 import {useState} from "react";
+import { useUserStore } from "../../../lib/userStore";
+import { useEffect } from "react";
+import {doc, onSnapshot, getDoc} from "firebase/firestore";
+import { db } from "../../../lib/firebase";
+
 const ChatList = () => {
+    const {currentUser} = useUserStore();
     const [addMode, setAddMode] = useState(false);
+    const [chats, setChats] = useState([]);
+
+    useEffect(() => {
+        const unSub = onSnapshot(
+            doc(db, "userchats", currentUser.id),
+            async (res) => {
+                const items = res.data().chats;
+
+                const promises = items.map(async(item)=>{
+                    const userDocRef = doc(db, "users", item.receiverId);
+                    const userDocSnap = await getDoc(docRef);
+                    const user = userDocSnap.data();
+                    return {...item, user};
+                });
+
+                const chatData = await Promise.all(promises);
+
+                setChats(chatData.sort((a,b) => b.updatedAt - a.updatedAt));
+            }
+        );
+        return () => {
+            unSub();
+        };
+    },[currentUser.id]);
+
+    console.log(chats);
+
     return (
         <div className="chatList">
             <div className="search">
@@ -14,93 +47,18 @@ const ChatList = () => {
                         onClick={() => setAddMode((prev) => !prev)}/>
             </div>
 
-            <div className="items">
-                <img src="./avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Sarah</span>
-                    <p>Hello</p>
+            {chats.map((chat) => (
+                <div className="items" key = {chat.chatId}>
+                    <img src="./avatar.png" alt=""/>
+                    <div className="texts">
+                        <span>Sarah</span>
+                        <p>{chat.lastMessage}</p>
+                    </div>
                 </div>
-            </div>
+            ))}
 
-            <div className="items">
-                <img src="./avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Sarah</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-
-            <div className="items">
-                <img src="./avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Sarah</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-
-            <div className="items">
-                <img src="./avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Sarah</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-
-            <div className="items">
-                <img src="./avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Sarah</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-
-            <div className="items">
-                <img src="./avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Sarah</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-
-            <div className="items">
-                <img src="./avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Sarah</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-
-            <div className="items">
-                <img src="./avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Sarah</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-
-            <div className="items">
-                <img src="./avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Sarah</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-
-            <div className="items">
-                <img src="./avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Sarah</span>
-                    <p>Hello</p>
-                </div>
-            </div>
-
-            <div className="items">
-                <img src="./avatar.png" alt=""/>
-                <div className="texts">
-                    <span>Sarah</span>
-                    <p>Hello</p>
-                </div>
-            </div>
+            
+            
             {addMode && <AddNewUser/>}
         </div>
     )
